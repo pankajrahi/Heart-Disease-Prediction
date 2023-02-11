34c22d8448c6809dd8077ffa4df007597221c852
pankajrahi <123239612+pankajrahi@users.noreply.github.com>
2023-02-11 14:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
 
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Churn Prediction ML App </h2>
+    <h2 style="color:white;text-align:center;">Heart Disease Prediction using ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
